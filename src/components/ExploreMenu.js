25c3d97f8d2db155a2dd9a9d9db92d69a8e79a30
Mayank Pandey <immayank7781@gmail.com>
2024-11-19 23:45:00 +0530

    Explore Menu

--- src/components/ExploreMenu.js
+++ src/components/ExploreMenu.js
@@ -2,6 +2,13 @@ import React from 'react'
 import menu from "../assets/menu.png"
 import {menuData} from "../../src/data"
 import ScrollAnimation from "react-animate-on-scroll"
+import { motion } from "framer-motion"
+
+
+const exploreMenu = {
+    visible: { opacity: 1, x:0},
+    hidden: { opacity: 0 , x:500},
+};
 
 const ExploreMenu = ({category, setCategory}) => {
 
@@ -29,15 +36,16 @@ const ExploreMenu = ({category, setCategory}) => {
         <div className='flex justify-between mt-5'>
             {menuData.map((item, index)=>{
                 return (
-                    <ScrollAnimation key={index} className='flex flex-col items-center cursor-pointer' onClick={()=>categoryHandler(item.menuName)} animateIn='fadeIn'
-                    animateOut='fadeOut'>
-                        <div className={category===item.menuName ? "w-[100px] h-[100px] rounded-full overflow-hidden transition-all border-[3px] border-[#FC8A06]" : "w-[100px] h-[100px] rounded-full overflow-hidden"}>
-                            <img src={item.menuImg} className='w-[100%] h-[100%] object-cover'/>
-                        </div>
-                        <span className='font-semibold'>
-                            {item.menuName}
-                        </span>
-                    </ScrollAnimation>
+                    <div>
+                        <motion.div key={index} className='flex flex-col items-center cursor-pointer' onClick={()=>categoryHandler(item.menuName)} initial="hidden" animate="visible" variants={exploreMenu} transition={{duration : 1+(index/10)}}>
+                            <div className={category===item.menuName ? "w-[100px] h-[100px] rounded-full overflow-hidden transition-all border-[3px] border-[#FC8A06]" : "w-[100px] h-[100px] rounded-full overflow-hidden"}>
+                                <img src={item.menuImg} className='w-[100%] h-[100%] object-cover'/>
+                            </div>
+                            <span className='font-semibold'>
+                                {item.menuName}
+                            </span>
+                        </motion.div>
+                    </div>
                 );
             })}
         </div>
